1e6164d7bfd0884f602a95725c8dce373b2c8e6b
Aditya Raj <aditya.rajb@gmail.com>
2022-06-14 15:57:39 +0530

    add signup

--- start/routes.ts
+++ start/routes.ts
@@ -19,6 +19,8 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import User from 'App/Models/User'
 
 Route.get('/', async () => {
   return { hello: 'world' }
@@ -27,8 +29,16 @@ Route.get('/', async () => {
 Route.resource('products', 'ProductsController')
 
 Route.post('login', async ({ auth, request, response }) => {
-  const email = request.input('email')
-  const password = request.input('password')
+  if (auth.isLoggedIn) {
+    return response.unauthorized({ err: "Already logged in" })
+  }
+
+  const loginSchema = schema.create({
+    email: schema.string([rules.email()]),
+    password: schema.string()
+  })
+
+  const { email, password } = await request.validate({ schema: loginSchema })
 
   try {
     const token = await auth.use('api').attempt(email, password)
@@ -36,4 +46,23 @@ Route.post('login', async ({ auth, request, response }) => {
   } catch {
     return response.badRequest({ err: 'Invalid credentials' })
   }
+})
+
+Route.post('signup', async ({ auth, request, response }) => {
+  if (auth.isLoggedIn) {
+    return response.unauthorized({ err: "Already logged in" })
+  }
+
+  const signupSchema = schema.create({
+    name: schema.string([rules.maxLength(150)]),
+    email: schema.string([rules.email()]),
+    password: schema.string()
+  })
+
+  const { name, email, password } = await request.validate({ schema: signupSchema })
+
+  const newUser = await User.create({ name, email, password })
+  const token = await auth.use('api').generate(newUser)
+
+  return { token, created: newUser }
 })
